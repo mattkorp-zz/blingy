34e7b663763a0ba396e0c96e8ac8935edd6dbe40
Harrison Powers <fijimunkii@gmail.com>
2013-11-25 01:00:14 -0500

    makes accounts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
     if current_user
       @user = current_user
       @accounts = current_user.accounts
+      @new_account = Account.new(user_id: @user.id)
     else
       @user = User.new
     end
